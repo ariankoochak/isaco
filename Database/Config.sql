f8f30facb333d54dc625e63a134560662031e2b2
Arian <45326611+ariankoochak@users.noreply.github.com>
2023-12-20 13:20:11 +0330

    add datas to config db

--- Database/Config.sql
+++ Database/Config.sql
@@ -198,3 +198,558 @@ VALUES (
     'Others',
     '09121234533'
 ); 
+
+
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۶ تیپ ۲',
+	'TU3',
+    '206',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۶ تیپ ۵',
+	'TU5',
+    '206',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۶ تیپ ۳',
+	'TU3',
+    '206',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۶SD',
+	'TU5',
+    '206SD',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷ تیپ ۲',
+	'TU3',
+    '207',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷ دنده ای',
+	'TU5',
+    '207',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷ اتوماتیک',
+	'TU5',
+    '207',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷ پاناروما دستی',
+	'TU5',
+    '207',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷ پاناروما اتوماتیک',
+	'TU5',
+    '207',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷MC',
+	'TU5',
+    '207',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۲۰۷SD',
+	'TU5',
+    '207SD',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۴۰۵',
+	'XU7',
+    '405',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'۴۰۵ TU5',
+	'TU5',
+    '405',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;Red;Silver;White'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'2008',
+	'Peugeot',
+    '2008',
+    'MetalicBlack;MetalicGray;MetalicSilver;RoyalBlue;VenetianRed;White;White2Layer'
+); 
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'آریسان',
+	'XU7P',
+    'Arisun',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;Charcoal;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'آریسان',
+	'XU7',
+    'Arisun',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;Charcoal;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'دنا معمولی',
+	'EF7',
+    'Dena',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'دنا پلاس ساده',
+	'EF7',
+    'DenaPlus',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'دنا پلاس توربو دستی',
+	'EF7 Turbo',
+    'DenaPlus',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'دنا پلاس توربو 6 دنده',
+	'EF7 Turbo',
+    'DenaPlus',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'دنا پلاس توربو اتوماتیک',
+	'EF7 Turbo',
+    'DenaPlus',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'دنا پلاس جوانان ',
+	'EFP',
+    'DenaPlus',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'H30 Cross',
+	'TU5',
+    'H30Cross',
+    'Brown;Black;Blue;Orange;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'S5 هایما ',
+	'Haima',
+    'HaimaS5',
+    'Brown;Black;Blue;Green;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'S7 هایما ',
+	'Haima',
+    'HaimaS7',
+    'Brown;Black;Blue;Charcoal;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'پارس معمولی',
+	'XU7',
+    'Pars',
+    'BluishBlue;Black;Green;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'پارس ELX',
+	'XU7P',
+    'Pars',
+    'BluishBlue;Black;Green;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'پارس TU5',
+	'TU5',
+    'Pars',
+    'BluishBlue;Black;Green;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'رانا معمولی',
+	'TU5',
+    'Runna',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'رانا پلاس',
+	'TU5',
+    'Runna',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'رانا پلاس 6 دنده',
+	'TU5',
+    'Runna',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'رانا پلاس پاناروما',
+	'TU5',
+    'Runna',
+    'Beige;Black;CaspianBlue;CherryRed;Gray;Red;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'S30',
+	'TU5',
+    'S30',
+    'BluishBlue;Black;Blue;VenetianRed;Gray;Red;Silver;White;Brown'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سمند معمولی',
+	'XU7',
+    'SamandLX',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سمند EF7',
+	'EF7',
+    'SamandLX',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سمند TU5',
+	'TU5',
+    'SamandLX',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سمندSE',
+	'XU7',
+    'SamandSE',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سورن معمولی',
+	'XU7',
+    'Soren',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سورن توربو',
+	'EF7 Turbo',
+    'Soren',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سورن EF7',
+	'EF7',
+    'Soren',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'سورن پلاس',
+	'EF7',
+    'Soren',
+    'Beige;Black;BluishBlue;MetalicGray;Gray;RoyalBlue;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'تارا دستی',
+	'EF7',
+    'Tara',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'تارا اتوماتیک',
+	'EF7',
+    'Tara',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'تارا V1',
+	'EF7',
+    'Tara',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'تارا V4',
+	'EF7P',
+    'Tara',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
+
+INSERT INTO isaco.CarsList(
+    CarName,
+    Engine,
+    CarCategory,
+    Colors)
+VALUES (
+	N'تارا V3',
+	'EF7',
+    'Tara',
+    'Brown;Black;BluishBlue;RoyalBlue;Gray;VenetianRed;Silver;White'
+);
